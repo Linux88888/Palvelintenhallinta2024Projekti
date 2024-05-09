96d899303670543835965b52cbfeb88f0e27c137
Linux88888 <143414956+Linux88888@users.noreply.github.com>
2024-05-09 14:49:34 +0300

    Update minion.sh

--- Skriptit/minion.sh
+++ Skriptit/minion.sh
@@ -5,8 +5,8 @@ sudo apt-get update
 sudo apt-get install salt-minion -y
 
 # Konfiguroi Salt Minion käyttämään t001 masterina
-# Tarkista, onko master-asetus jo olemassa, ja päivitä se. Jos ei, lisää uusi rivi.
-sudo sed -i '/^master:/c\master: 192.168.88.101' /etc/salt/minion || echo 'master: 192.168.88.101' | sudo tee -a /etc/salt/minion
+# Lisää uusi rivi minionin tekstitiedoston loppuun.
+echo 'master: 192.168.88.101' | sudo tee -a /etc/salt/minion
 
 # Käynnistä Salt Minion uudelleen
 sudo systemctl restart salt-minion
