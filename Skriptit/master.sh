3954f3995ceef91be6808d18056c7aa20981cdd4
Linux88888 <143414956+Linux88888@users.noreply.github.com>
2024-05-12 01:54:01 +0300

    Update master.sh

--- Skriptit/master.sh
+++ Skriptit/master.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
+
 # Päivitä paketit
 sudo apt-get update
 # Asenna Salt Master
 sudo apt-get install salt-master -y
+
+# Käynnistä Salt Master palvelu
+sudo systemctl start salt-master
+sudo systemctl enable salt-master
 
 # Odotetaan, että Minionit lähettävät avaimensa
 sleep 60  # Odotetaan 60 sekuntia, jotta minionit ehtivät käynnistyä ja lähettää avaimensa
@@ -10,5 +15,16 @@ sleep 60  # Odotetaan 60 sekuntia, jotta minionit ehtivät käynnistyä ja lähe
 # Hyväksy kaikki Minionien avaimet automaattisesti
 sudo salt-key -A -y
 
-# Testaa yhteys
+# Lataa tarvittavat state-tiedostot GitHubista
+sudo mkdir -p /srv/salt
+cd /srv/salt
+sudo wget https://github.com/Linux88888/Palvelintenhallinta2024Projekti/archive/refs/heads/main.zip
+sudo unzip main.zip
+sudo mv Palvelintenhallinta2024Projekti-main/* .
+sudo rm -rf Palvelintenhallinta2024Projekti-main main.zip
+
+# Testaa yhteys Minioneihin
 sudo salt '*' cmd.run 'whoami'
+
+# Aja esimerkiksi webkehittäjäntyökalujen asennus
+sudo salt '*' state.apply Paketit/webkehittäjäntyökalut
