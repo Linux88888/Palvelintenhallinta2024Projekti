fa7156e58c124428a3f70cdedd0e2c55604dd1f1
Linux88888 <143414956+Linux88888@users.noreply.github.com>
2024-05-13 23:54:22 +0300

    Update master.sh

--- Skriptit/master.sh
+++ Skriptit/master.sh
@@ -11,4 +11,13 @@ sleep 60  # Odotetaan 60 sekuntia, jotta minionit ehtivät käynnistyä ja lähe
 sudo salt-key -A -y
 
 # Testaa yhteys
-sudo salt '*' cmd.run 'whoami' // mitä tähän voisi lisätä jotta saataisiin salt tilat ajettavaksi minionille githubin kautta
+sudo salt '*' cmd.run 'whoami'
+
+# Luo hakemisto Salt-tilatiedostoille, jos sitä ei ole
+sudo mkdir -p /srv/salt
+
+# Lataa tilatiedosto GitHubista
+sudo wget -O /srv/salt/webkehittäjäntyökalut.sls https://raw.githubusercontent.com/Linux88888/Palvelintenhallinta2024Projekti/main/Paketit/webkehitt%C3%A4j%C3%A4nty%C3%B6kalut.sls
+
+# Aja Salt state Minionille t002
+sudo salt 't002' state.apply webkehittäjäntyökalut
